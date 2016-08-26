c588c33e56d8477d7b038006dd4272c3da92f1cb
Nhat Hoang <hoangnh092185@gmail.com>
2016-08-26 13:02:14 -0700

    Add aditional buttons for vegies and meat options when click.

--- js/scripts.js
+++ js/scripts.js
@@ -7,11 +7,6 @@ function Pie(size, sauce){
 };
 
 
-Pie.prototype.fiberCost = function(){
-  return this.pizzaFiber.length * 1;
-  console.log(fiberCost);
-};
-
 Pie.prototype.sizeCost = function(){
   var pizzaCost = 0;
   if (this.pizzaSize === "Family"){
@@ -24,47 +19,66 @@ Pie.prototype.sizeCost = function(){
     pizzaCost = 6;
   }
 };
+Pie.prototype.fiberCost = function(){
+  return parseInt(this.pizzaFiber.length) * 1;
+  console.log(fiberCost);
+};
 
 Pie.prototype.proteinCost = function(){
-  return this.pizzaProtein.length * 2;
+  return parseInt(this.pizzaProtein.length) * 2;
   console.log(proteinCost);
 };
 
 Pie.prototype.totalCost = function(){
-  this.totalAmount = fiberCost + proteinCost + sizeCost;
+  this.totalAmount = (fiberCost + proteinCost + sizeCost);
 };
 
 
 $(document).ready(function(){
+  // add Fiber option begin
+  $("#pie-veggies").click(function(){
+    $("#new-pie-vegies").append(<div id="new-pie-vegies">
+                              '<label class="new-pie-vegies clone" for="">Select topping: $1/each</label>'+
+                              '<select class="new-pie-vegies clone" name="">'+
+                                '<option>Pineapple</option>'+
+                                '<option>Mushrooms</option>'+
+                                '<option>Spinach</option>'+
+                                '<option>Onions</option>'+
+                                '<option>Sun Dried Tomatoes</option>'+
+                                '<option>Artichoke Hearts</option>'+
+                              '</select>'+
+                              '</div>')
+  })
+    // add Fiber Option end
+  // add Protein option begin
+  $("div.pie-meat").click(function(){
+    $("div.pie-meat").append('<label class="pie-meat" for="">Select topping: $1/each</label>'+
+                              '<select class="pie-meat" name="">'+
+                              '<option>Pepperoni</option>'+
+                              '<option>Salami</option>'+
+                              '<option>BBQ Chicken</option>'+
+                              '<option>Italian Sausage</option>'+
+                              '<option>Canadian Style Bacon</option>'+
+                            '</select>')
+  });
+  // add Protein option end
 
-  submitform begin
-  $("form.form-group").submit(function(){
-    var pizzaSize = $("").val();
-    var pizzaSauce = $("").val();
+  // submitform begin
+  $("form.display-option").submit(function(event){
+    event.preventDefault();
 
-    var orderedPizza = new Pie(size, sauce);
+    var pieSize = $("select.pie-size").val();
+    var pieSauce = $("select.pie-sauce").val();
+
+    var orderedPizza = new Pie(pieSize, pieSauce);
 
     $(".addedToppings").each(function(){
-      var pizzaVegies = ("Mushroom", "Spinach", "Artichoke").val();
-      var pizzaMeats = ("Sausage", "Pepperoni", "Chicken").val();
+      var pieVegies = $("select.pie-vegies").val();
+      var pieMeats = $("select.pie-meat").val();
       orderedPizza.pizzaFiber.push(pizzaVegies);
       orderedPizza.pizzaProtein.push(pizzaMeat);
     })
-
   });
-
-  Submitform end
-
-  add topping option begin
-  $("button.pie-veggies").click(function(){
-    $("div").append()
-  })
-  add Protein option end
-  $("button.pie-meat").click(function(){
-    $("div.pie-meat").append()
-  })
-  add Protein option end
-
-
-
-})
+  // Submitform end
+});
+// document end
