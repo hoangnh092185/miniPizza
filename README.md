b617dd05e7a2f0a7a62877e52d82bd80e283a05d
Nhat Hoang <hoangnh092185@gmail.com>
2016-08-28 23:58:16 -0700

    update readme file

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-* Mini Pizza
+# MiniPizza
+
+## Input forms to connect business logic using Javascript, 8/26/16
+
+#### By **Nhat Hoang**
+
+## Description
+
+Uses html, Javascript, jquery, and bootstrap to create user interface between the front-end and the business logic.
+
+## Setup/Installation Requirements
+
+* Just need a web browser
+
+## Technologies Used
+
+* HTML
+* Bootstrap
+* Jquery
+* Javascript
+
+##link##
+#### https://github.com/hoangnh092185/miniPizza
+#### https://github.com/hoangnh092185/miniPizza/tree/gh-pages
+### License
+
+*GPL*
+
+Copyright &copy; 2016 **Nhat Hoang**
--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
 function Pie(size, sauce, fiber, protein){
   this.pizzaSize = size;
   this.pizzaSauce = sauce;
-  this.pizzaFiber = [];
-  this.pizzaProtein = [];
+  this.pizzaFiber = fiber;
+  this.pizzaProtein = protein;
   this.totalAmount = 0;
-<<<<<<< 27adecf96d58eaf4bec69b7264b66fd8c7ac08cf
-  console.log(this.pizzaSize, this.pizzaSauce, this.pizzaFiber, this.pizzaProtein)
-=======
->>>>>>> initial commit to branch.
+
+
+
 };
 
 Pie.prototype.sizeCost = function(){
@@ -18,27 +17,27 @@ Pie.prototype.sizeCost = function(){
   }else if (this.pizzaSize === "Medium"){
     return 10;
   }else {
-    return sizeCost = 6;
+    return 6;
   }
 };
 
-Pie.prototype.fiberCost = function(){
-  return ((this.pizzaFiber.length)* 1);
+var orderedList = function(){
+    foreach(Pie) {
+    $("#toppingList").append("<li>"+Pie()+"</li>");
+  // };
 };
 
-Pie.prototype.proteinCost = function(){
-  return (this.pizzaProtein.length * 2);
-  console.log(proteinCost);
-};
-
-Pie.prototype.totalCost = function(){
-  this.totalAmount = (fiberCost + proteinCost + sizeCost);
-};
-
+// Pie.prototype.fiberCost = function(){
+//   return 1;
+// };
+//
+// Pie.prototype.proteinCost = function(){
+//   return 2;
+// };
 
 $(document).ready(function(){
   // add Fiber option begin
-<<<<<<< 27adecf96d58eaf4bec69b7264b66fd8c7ac08cf
+
   // $("butt.new-pie-vegies original").click(function(){
   //   $("#pie-vegies").append('<div id="new-pie-vegies">'+
   //                             '<label class="new-pie-vegies clone" for="">Select topping: $1/each</label>'+
@@ -52,23 +51,21 @@ $(document).ready(function(){
   //                             '</select>'+
   //                             '</div>')
   // })
-=======
-  $("#addVegiesbtn").click(function(){
-    $("#pie-vegies").append(
-
-      '<div id="new-pie-vegies">'+
-                              '<label class="new-pie-vegies clone" for="">Select topping: $1/each</label>'+
-                              '<select class="new-pie-vegies clone" name="">'+
-                                '<option>Pineapple</option>'+
-                                '<option>Mushrooms</option>'+
-                                '<option>Spinach</option>'+
-                                '<option>Onions</option>'+
-                                '<option>Sun Dried Tomatoes</option>'+
-                                '<option>Artichoke Hearts</option>'+
-                              '</select>'+
-                              '</div>')
-  });
->>>>>>> initial commit to branch.
+  // $("#addVegiesbtn").click(function(){
+  //   $("#pie-vegies").append(
+  //
+  //     '<div id="new-pie-vegies">'+
+  //                             '<label class="new-pie-vegies clone" for="">Select topping: $1/each</label>'+
+  //                             '<select class="new-pie-vegies clone" name="">'+
+  //                               '<option>Pineapple</option>'+
+  //                               '<option>Mushrooms</option>'+
+  //                               '<option>Spinach</option>'+
+  //                               '<option>Onions</option>'+
+  //                               '<option>Sun Dried Tomatoes</option>'+
+  //                               '<option>Artichoke Hearts</option>'+
+  //                             '</select>'+
+  //                             '</div>')
+  // });
     // add Fiber Option end
   // add Protein option begin
   // $("#pie-meat").click(function(){
@@ -84,26 +81,30 @@ $(document).ready(function(){
   // add Protein option end
 
   // submitform begin
+
+
   $("form.display-option").submit(function(event){
     event.preventDefault();
 
     var pieSize = $("select.pie-size").val();
     var pieSauce = $("select.pie-sauce").val();
-    // var pieVegies = $("select.new-pie-vegies").val();
-    // var pieMeats = $("select.new-pie-meat").val();
+    var pieVegies = $("select.new-pie-vegies").val();
+    var pieMeats = $("select.new-pie-meat").val();
 
-    var orderedPizza = new Pie(pieSize, pieSauce);
-    console.log(orderedPizza.fiberCost());
+    var orderedPizza = new Pie(pieSize, pieSauce, pieVegies, pieMeats);
+    console.log(orderedPizza)
+    orderedList ();
+    // orderedPrice ();
 
     // var orderedPizza = new Pie(pieSize, pieSauce, pieVegies, pieMeats);
 
-    $("div.addedToppings").each(function(){
-      var pieVegies = $("select.new-pie-vegies").val();
-      var pieMeats = $("select.new-pie-meat").val();
-      orderedPizza.pizzaFiber.push(pizzaVegies);
-      orderedPizza.pizzaProtein.push(pizzaMeat);
+    // $("div.addedToppings").each(function(){
+    //   var pieVegies = $("select.new-pie-vegies").val();
+    //   var pieMeats = $("select.new-pie-meat").val();
+    //   orderedPizza.pizzaFiber.push(pizzaVegies);
+    //   orderedPizza.pizzaProtein.push(pizzaMeat);
 
-    })
+    // })
   });
   // Submitform end
 });
