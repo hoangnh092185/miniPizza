27adecf96d58eaf4bec69b7264b66fd8c7ac08cf
Nhat Hoang <hoangnh092185@gmail.com>
2016-08-28 22:47:31 -0700

    Will calculate cost for pizza Size.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,30 @@
-function Pie(size, sauce){
+function Pie(size, sauce, fiber, protein){
   this.pizzaSize = size;
   this.pizzaSauce = sauce;
   this.pizzaFiber = [];
   this.pizzaProtein = [];
   this.totalAmount = 0;
-  console.log(this.pizzaSize);
+  console.log(this.pizzaSize, this.pizzaSauce, this.pizzaFiber, this.pizzaProtein)
 };
-
 
 Pie.prototype.sizeCost = function(){
-  var pizzaCost = 0;
   if (this.pizzaSize === "Family"){
-    pizzaCost = 20;
-  }if (this.pizzaSize === "Large"){
-    pizzaCost = 16;
-  }if (this.pizzaSize === "Medium"){
-    pizzaCost = 10;
-  }if (this.pizzaSize === "Small"){
-    pizzaCost = 6;
+    return 20;
+  }else if (this.pizzaSize === "Large"){
+    return 16;
+  }else if (this.pizzaSize === "Medium"){
+    return 10;
+  }else {
+    return sizeCost = 6;
   }
 };
+
 Pie.prototype.fiberCost = function(){
-  return parseInt(this.pizzaFiber.length) * 1;
-  console.log(fiberCost);
+  return ((this.pizzaFiber.length)* 1);
 };
 
 Pie.prototype.proteinCost = function(){
-  return parseInt(this.pizzaProtein.length) * 2;
+  return (this.pizzaProtein.length * 2);
   console.log(proteinCost);
 };
 
@@ -37,31 +35,31 @@ Pie.prototype.totalCost = function(){
 
 $(document).ready(function(){
   // add Fiber option begin
-  $("butt.new-pie-vegies original").click(function(){
-    $("#pie-vegies").append('<div id="new-pie-vegies">'+
-                              '<label class="new-pie-vegies clone" for="">Select topping: $1/each</label>'+
-                              '<select class="new-pie-vegies clone" name="">'+
-                                '<option>Pineapple</option>'+
-                                '<option>Mushrooms</option>'+
-                                '<option>Spinach</option>'+
-                                '<option>Onions</option>'+
-                                '<option>Sun Dried Tomatoes</option>'+
-                                '<option>Artichoke Hearts</option>'+
-                              '</select>'+
-                              '</div>')
-  })
+  // $("butt.new-pie-vegies original").click(function(){
+  //   $("#pie-vegies").append('<div id="new-pie-vegies">'+
+  //                             '<label class="new-pie-vegies clone" for="">Select topping: $1/each</label>'+
+  //                             '<select class="new-pie-vegies clone" name="">'+
+  //                               '<option>Pineapple</option>'+
+  //                               '<option>Mushrooms</option>'+
+  //                               '<option>Spinach</option>'+
+  //                               '<option>Onions</option>'+
+  //                               '<option>Sun Dried Tomatoes</option>'+
+  //                               '<option>Artichoke Hearts</option>'+
+  //                             '</select>'+
+  //                             '</div>')
+  // })
     // add Fiber Option end
   // add Protein option begin
-  $("#pie-meat").click(function(){
-    $("div.new-pie-meat").append('<label class="pie-meat" for="">Select topping: $1/each</label>'+
-                              '<select class="pie-meat" name="">'+
-                              '<option>Pepperoni</option>'+
-                              '<option>Salami</option>'+
-                              '<option>BBQ Chicken</option>'+
-                              '<option>Italian Sausage</option>'+
-                              '<option>Canadian Style Bacon</option>'+
-                            '</select>')
-  });
+  // $("#pie-meat").click(function(){
+  //   $("div.new-pie-meat").append('<label class="pie-meat" for="">Select topping: $1/each</label>'+
+  //                             '<select class="pie-meat" name="">'+
+  //                             '<option>Pepperoni</option>'+
+  //                             '<option>Salami</option>'+
+  //                             '<option>BBQ Chicken</option>'+
+  //                             '<option>Italian Sausage</option>'+
+  //                             '<option>Canadian Style Bacon</option>'+
+  //                           '</select>')
+  // });
   // add Protein option end
 
   // submitform begin
@@ -70,14 +68,20 @@ $(document).ready(function(){
 
     var pieSize = $("select.pie-size").val();
     var pieSauce = $("select.pie-sauce").val();
+    // var pieVegies = $("select.new-pie-vegies").val();
+    // var pieMeats = $("select.new-pie-meat").val();
 
     var orderedPizza = new Pie(pieSize, pieSauce);
+    console.log(orderedPizza.fiberCost());
 
-    $(".addedToppings").each(function(){
-      var pieVegies = $("select.pie-vegies").val();
-      var pieMeats = $("select.pie-meat").val();
+    // var orderedPizza = new Pie(pieSize, pieSauce, pieVegies, pieMeats);
+
+    $("div.addedToppings").each(function(){
+      var pieVegies = $("select.new-pie-vegies").val();
+      var pieMeats = $("select.new-pie-meat").val();
       orderedPizza.pizzaFiber.push(pizzaVegies);
       orderedPizza.pizzaProtein.push(pizzaMeat);
+
     })
   });
   // Submitform end
