74400961d4a1123427cc993ccfb462a1eb8d9e2b
hoangnh092185 <hoangnh092185@gmail.com>
2016-08-31 10:45:34 -0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,60 @@
+function Pizza(size, sauce, veggies, proteins){
+  this.pizzaSize = size;
+  this.pizzaSauce = sauce;
+  this.topVeggies = veggies;
+  this.topProteins = proteins;
+};
+
+Pizza.prototype.sizeCost = function(cost2){
+    var cost2 = 0;
+  if (this.pizzaSize === "Family"){
+    return cost2 += 20; ;
+  }else if (this.pizzaSize === "Large"){
+    return cost2 += 16; ;
+  }else if (this.pizzaSize === "Medium"){
+    return cost2 += 10; ;
+  }else {
+    return cost2 += 6;
+  }
+};
+
+Pizza.prototype.arrayVeggieCost = function(veggCost) {
+  return veggCost = (this.topVeggies.length)*0.5;
+}
+Pizza.prototype.arrayProteinCost = function(protCost) {
+  return protCost = (this.topProteins.length)*2;
+}
+
+var orderedList = function(list){
+  $("#toppingList").append("<li>"+"Your toppings is: "+ list +"</li>");
+};
+var orderedPrice = function(cost){
+    $("#totalPizzaCost").append("<li>"+"Your total is :$" + cost +"</li>");
+};
+
+$(document).ready(function(){
+
+  $("form.display-option").submit(function(event){
+    event.preventDefault();
+
+    var pizzaSize = $("select.pie-size").val();
+    var pizzaSauce = $("select.pie-sauce").val();
+
+    var pizzaVeggies = [];
+    $('input[name="vegies"]:checked').each(function() {
+        pizzaVeggies.push(this.value);
+      });
+
+    var pizzaProteins = [];
+    $('input[name="proteins"]:checked').each(function() {
+        pizzaProteins.push(this.value);
+      });
+
+    var orderedPizza = new Pizza(pizzaSize, pizzaSauce, pizzaVeggies, pizzaProteins);
+
+    orderedList (orderedPizza.pizzaSize+orderedPizza.pizzaSauce+orderedPizza.topVeggies+orderedPizza.topProteins);
+
+    orderedPrice (orderedPizza.sizeCost()+orderedPizza.arrayVeggieCost()+orderedPizza.arrayProteinCost());
+  });
+
+});
